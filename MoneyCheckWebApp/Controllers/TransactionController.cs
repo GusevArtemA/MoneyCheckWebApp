2b81a3d4ecf4d2c6c2b2730d7c8ec44b103ccd8e
ArtHack <mrtaktplay@gmail.com>
2021-12-27 23:49:47 +0300

    Да

---
+++ MoneyCheckWebApp/Controllers/TransactionController.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MoneyCheckWebApp.Models;
+using MoneyCheckWebApp.Types.Purchases;
+using MoneyCheckWebApp.Extensions;
+
+namespace MoneyCheckWebApp.Controllers
+{
+    [Route("api/transactions")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private MoneyCheckDbContext _context;
+
+        public TransactionController(MoneyCheckDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("add-purchase")]
+        public async Task<IActionResult> AddPurchaseAsync([FromBody] PurchaseType purchase)
+        {
+            if ((purchase.BoughtAt - DateTime.Now) > TimeSpan.FromDays(1))
+            {
+                return BadRequest("Date error");
+            }
+            else if (purchase.CategoryId == default)
+            {
+                return BadRequest("Category not specified");
+            }
+
+            var addPurchase = new Purchase()
+            {
+                BoughtAt = purchase.BoughtAt,
+                Amount = purchase.Amount,
+                CategoryId = purchase.CategoryId,
+                CustomerId = this.ExtractUser().Id
+            };
+
+
+            if (_context.Categories.FirstOrDefault(x => x.Id == purchase.CategoryId) == null)
+            {
+                BadRequest("Category error");
+            }
+
+            await _context.Purchases.AddAsync(addPurchase);
+
+            this.ExtractUser().Balance -= purchase.Amount;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(purchase);
+        }
+
+        [HttpPost]
+        [Route("remove-purchase")]
+        public async Task<IActionResult> RemovePurchaseAsync(long id)
+        {
+            var extractedUser = this.ExtractUser();
+            var foundPurchase = _context.Purchases.SingleOrDefault(x => x.Id == id && x.CustomerId == extractedUser.Id);
+            if (foundPurchase != null)
+            {
+                extractedUser.Balance += foundPurchase.Amount;
+                _context.Purchases.Remove(foundPurchase);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                return BadRequest("");
+            }
+
+            return Ok();
+        }
+
+
+
+        [HttpPost]
+        [Route("edit-purchase")]
+        public async Task<IActionResult> EditPurchaseAsync([FromBody] PurchaseType purchase)
+        {
+            var user = this.ExtractUser();
+            var beforeEditedPurchase = _context.Purchases.FirstOrDefault(x => x.Id == purchase.Id && x.CustomerId == user.Id);
+            
+            if (beforeEditedPurchase != null)
+            {
+                user.Balance += beforeEditedPurchase.Amount;
+                user.Balance -= purchase.Amount;
+
+                beforeEditedPurchase.Id = purchase.Id;
+                beforeEditedPurchase.Amount = purchase.Amount;
+                beforeEditedPurchase.CategoryId = purchase.CategoryId;
+
+
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                return BadRequest("");
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("add-category")]
+        public async Task<IActionResult> AddCategoryAsync([FromBody] CategoryType category)
+        {
+            var lastCategory = _context.Categories.LastOrDefault();
+
+            if (lastCategory == null || lastCategory.Id < category.SubCategoryId || _context.Categories.FirstOrDefault(x => x.Id == category.SubCategoryId) == null)
+                return BadRequest();
+
+
+            Category addCategory = new Category()
+            {
+                CategoryName = category.CategoryName,
+                SubCategoryId = category.SubCategoryId
+            };
+
+            await _context.Categories.AddAsync(addCategory);
+            await _context.SaveChangesAsync();
+
+
+            return Ok(_context.Categories.Find(addCategory).Id);
+
+        }
+
+        [HttpPost]
+        [Route("get-purchases")]
+        public IActionResult GetPurchases()
+        {
+            long userId = this.ExtractUser().Id;
+            var purchases = _context.Purchases.Where(x => x.CustomerId == userId);
+            List<PurchaseType> list = new List<PurchaseType>();
+            foreach (var item in purchases)
+            {
+                list.Add(new PurchaseType()
+                {
+                    Amount = item.Amount,
+                    BoughtAt = item.BoughtAt,
+                    CategoryId = item.CategoryId,
+                    Id = item.Id,
+                    Longitude = item.Longitude == default ? 0 : (decimal)item.Longitude,
+                    Latitude = item.Latitude == default ? 0 : (decimal)item.Latitude
+                });
+            }
+
+            return Ok(list);
+        }
+
+
+    }
+}
---
+++ MoneyCheckWebApp/Types/Purchases/PurchaseType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyCheckWebApp.Types.Purchases
+{
+    public class PurchaseType
+    {
+        public long Id { get; set; }
+        public DateTime BoughtAt { get; set; }
+        public long Amount { get; set; }
+        public long CategoryId { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+    }
+}
