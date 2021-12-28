75a975645806ac1550e85ad6589eb058853f1a25
ArtHack <mrtaktplay@gmail.com>
2021-12-28 13:06:11 +0300

    update v2

--- MoneyCheckWebApp/Types/Debts/DebtorType.cs
+++ MoneyCheckWebApp/Types/Debts/DebtorType.cs
@@ -8,6 +8,6 @@ namespace MoneyCheckWebApp.Types.Debtors
     public class DebtorType
     {
         public string Name { get; set; }
-        public long NaturalMaskId { get; set; }
+        public long? NaturalMaskId { get; set; }
     }
 }
