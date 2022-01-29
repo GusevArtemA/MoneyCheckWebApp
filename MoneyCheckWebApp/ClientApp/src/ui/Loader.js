aa43143d24a5cbff5591013650fa36413541dcdc
Denis Voitenko <denis@denvot.xyz>
2022-01-30 00:54:26 +0300

    Loader

--- MoneyCheckWebApp/ClientApp/src/ui/Loader.js
+++ MoneyCheckWebApp/ClientApp/src/ui/Loader.js
@@ -1,8 +1,10 @@
 import React from "react";
+import LoadingIcon from "../assets/images/animated/loader.svg";
 
 export class Loader extends React.Component {
     render() {
-        return (
+        return <object width="100" type="image/svg+xml" data={LoadingIcon}>svg-animation</object>
+        /*return (
             <div className="d-flex flex-row loader justify-content-around">
                 <div className="first-circle">
                 </div>
@@ -11,6 +13,6 @@ export class Loader extends React.Component {
                 <div className="third-circle">
                 </div>
             </div>
-        );
+        );*/
     }
 }
