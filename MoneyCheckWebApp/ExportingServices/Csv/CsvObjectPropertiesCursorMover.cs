576cdd55965d0987f4a71b2d565cdc1eed61eb62
Denis <dv.voitenko@gmail.com>
2022-01-29 15:14:05 +0300

    Csv exporting providers support

---
+++ MoneyCheckWebApp/ExportingServices/Csv/CsvObjectPropertiesCursorMover.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Reflection;
+using System.Text;
+
+namespace MoneyCheckWebApp.ExportingServices.Csv
+{
+    public class CsvObjectPropertiesCursorMover
+    {
+        private readonly object _obj;
+        private PropertyInfo[] _props;
+        private int _point = 0;
+
+        public CsvObjectPropertiesCursorMover(object obj)
+        {
+            _obj = obj;
+            _props = _obj.GetType().GetProperties();
+
+            if (_props.Length == 0)
+            {
+                throw new ArgumentException($"There is no public props in {obj.GetType()}");
+            }
+        }
+
+        public MoveResult MoveNext()
+        {
+            if (_point >= _props.Length)
+            {
+                return MoveResult.EndResult();
+            }
+
+            var buffer = new StringBuilder();
+
+            buffer.Append(GetPropValueInString(_props[_point]));
+
+            if (_point < _props.Length - 1)
+            {
+                buffer.Append(',');
+            }
+
+            _point++;
+            
+            return MoveResult.WithResult(buffer.ToString());
+        }
+
+        private string GetPropValueInString(PropertyInfo info) => info.GetValue(_obj)!.ToString() ?? String.Empty;
+    }
+}
