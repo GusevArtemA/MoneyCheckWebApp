fc4e86843ba9cf47334efa92bf1eecfcdee52123
Denis <denis@denvot.xyz>
2022-02-11 11:51:42 +0300

    Bug fixes

--- MoneyCheckWebApp/Controllers/ExportsController.cs
+++ MoneyCheckWebApp/Controllers/ExportsController.cs
@@ -14,16 +14,13 @@ namespace MoneyCheckWebApp.Controllers
     [Route("/api/exports")]
     public class ExportsController : ControllerBase
     {
-        private readonly MoneyCheckDbContext _context;
-
-        public ExportsController(MoneyCheckDbContext context)
+        public ExportsController()
         {
-            _context = context;
         }
 
         [HttpGet]
         [Route("csv-purchases")]
-        public async Task<IActionResult> GetCsvPurchases()
+        public IActionResult GetCsvPurchases()
         {
             var invoker = this.ExtractUser();
 
@@ -45,9 +42,11 @@ namespace MoneyCheckWebApp.Controllers
 
             var csvBytes = Encoding.UTF8.GetBytes(csv);
 
+            Response.ContentType = "UTF-8";
+            
             return File(csvBytes,
                 "application/csv",
-            $"Транзакции пользователя {invoker.Username} за {DateTime.Today.Year} год.csv");
+                $"Транзакции пользователя {invoker.Username} за {DateTime.Today.Year} год.csv");
         }
     }
 }
